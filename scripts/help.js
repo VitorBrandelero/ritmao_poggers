a5802b14c4f88b624f1f63d63c4d336795231e76
Vitor <vitorbrandelero99@gmail.com>
2024-11-12 21:09:52 -0300

    Ajustes finais

--- scripts/help.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Página de ajuda carregada.");
-    // Adicione funcionalidades ou eventos aqui, caso precise
-});
--- scripts/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Página principal carregada.");
-    // Adicione aqui funções ou eventos que deseja iniciar ao carregar a página principal
-});
